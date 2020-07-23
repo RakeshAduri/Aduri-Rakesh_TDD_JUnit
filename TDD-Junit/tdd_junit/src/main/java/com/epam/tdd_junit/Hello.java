cd44a660f34b8d61ce49b3cbe056f56d77071da0
RakeshAduri <60639774+RakeshAduri@users.noreply.github.com>
2020-07-23 11:17:45 +0530

    Add files via upload

---
+++ TDD-Junit/tdd_junit/src/main/java/com/epam/tdd_junit/Hello.java
@@ -0,0 +1,12 @@
+package com.epam.tdd_junit;
+/**
+ * Hello world!
+ *
+ */
+public class Hello 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
